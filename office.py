6eb821fd4eeef8825ebd03b339edb9cee2e9a651
SajidAlamQB <90610031+SajidAlamQB@users.noreply.github.com>
2022-09-16 14:27:09 +0100

    sphinx themes

--- office.py
+++ office.py
@@ -6,12 +6,12 @@ from employee import Employee
 
 class TheOffice:
     """A class representing an office that holds information about its employees and their sales.
-        It exposes methods make and view sales for each employee.
+    It exposes methods make and view sales for each employee.
 
-        Args:
-            regional_manager (str): The name of the employee.
-            employees (Set[Employee]): A set of employee objects.
-            location (str): The name of the region the office is located.
+    Args:
+        regional_manager (str): The name of the employee.
+        employees (Set[Employee]): A set of employee objects.
+        location (str): The name of the region the office is located.
     """
 
     def __init__(self, regional_manager: str, employees: Set[Employee], location: str):
@@ -21,10 +21,11 @@ class TheOffice:
         self.sales = defaultdict(int)
 
     def __str__(self) -> str:
-        """Represents the class object as a string providing a welcome message and the name of the `regional_manager`.
+        """Represents the class object as a string providing a welcome message
+        and the name of the `regional_manager`.
 
-            Returns:
-                str: Welcome message including the name of the `regional_manager`.
+        Returns:
+            str: Welcome message including the name of the `regional_manager`.
         """
         return f"Welcome to The Office! The boss here is {self.regional_manager}."
 
@@ -39,21 +40,21 @@ class TheOffice:
     def get_employee_sales(self, employee: Employee) -> int:
         """Method that returns the number of sales given an employee.
 
-            Args:
-                employee (Employee): The employee whose sales number we are interested in.
+        Args:
+            employee (Employee): The employee whose sales number we are interested in.
 
-            Returns:
-                int: The total number of sales the employee has made.
+        Returns:
+            int: The total number of sales the employee has made.
         """
         return self.sales[employee]
 
     def _fire_employee(self, employee: Employee) -> None:
         """Remove the `employee` from the `employees` set of the class object.
 
-            Args:
-                employee (Employee): The `employee` who is getting deleted.
+        Args:
+            employee (Employee): The `employee` who is getting deleted.
 
-            Raises:
-                KeyError: If `employee` is not found in the set of `employees`.
+        Raises:
+            KeyError: If `employee` is not found in the set of `employees`.
         """
         self.employees.remove(employee)
